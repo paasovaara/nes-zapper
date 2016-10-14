11b9712b07a25aa06d868ef16f572d2816deb653
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-10-14 23:07:53 +0300

    Added input pin for detector, but doesn't work this way.

--- src/zapper/zapper.ino
+++ src/zapper/zapper.ino
@@ -1,5 +1,6 @@
 const int outPinLed = 2;
 const int inPinTrigger = 7;
+const int inPinDetector = 4;
 
 int triggerPressed = 0;
 
@@ -11,6 +12,7 @@ void setup() {
   digitalWrite(outPinLed, LOW);
 
   pinMode(inPinTrigger, INPUT);
+  pinMode(inPinDetector, INPUT);
 
 }
 
@@ -20,8 +22,14 @@ void loop() {
   if (triggerPressed != triggerPrev) {
     Serial.print("Trigger state changed: ");
     Serial.println(triggerPressed);
+    if (triggerPressed) {
+      int detectorValue = digitalRead(inPinDetector);
+      
+      Serial.print("Detector: ");
+      Serial.println(detectorValue);
+    }
   }
-
+    
   int ledState = triggerPressed ? HIGH : LOW;
   digitalWrite(outPinLed, ledState);
 

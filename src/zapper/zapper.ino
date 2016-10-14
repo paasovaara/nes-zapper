9583dc0b1c70fc5990cb076b54425aa28d3aaae0
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-10-14 22:35:56 +0300

    Initial commit, blinking LED with zapper trigger

---
+++ src/zapper/zapper.ino
@@ -0,0 +1,28 @@
+const int outPinLed = 2;
+const int inPinTrigger = 7;
+
+int triggerPressed = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  pinMode(outPinLed, OUTPUT);
+  digitalWrite(outPinLed, LOW);
+
+  pinMode(inPinTrigger, INPUT);
+
+}
+
+void loop() {
+  int triggerPrev = triggerPressed;
+  triggerPressed = digitalRead(inPinTrigger);
+  if (triggerPressed != triggerPrev) {
+    Serial.print("Trigger state changed: ");
+    Serial.println(triggerPressed);
+  }
+
+  int ledState = triggerPressed ? HIGH : LOW;
+  digitalWrite(outPinLed, ledState);
+
+}

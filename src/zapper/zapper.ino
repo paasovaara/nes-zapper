45e217a8d4a79f02b8104e28a5370b663d03d0ed
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-10-21 21:40:28 +0300

    Read detector value to A0

--- src/zapper/zapper.ino
+++ src/zapper/zapper.ino
@@ -1,8 +1,9 @@
 const int outPinLed = 2;
 const int inPinTrigger = 7;
-const int inPinDetector = 4;
+const int inPinDetector = A0;
 
 int triggerPressed = 0;
+int detectorValue = 1023;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,9 +12,7 @@ void setup() {
   pinMode(outPinLed, OUTPUT);
   digitalWrite(outPinLed, LOW);
 
-  pinMode(inPinTrigger, INPUT);
-  pinMode(inPinDetector, INPUT);
-
+  pinMode(inPinTrigger, INPUT_PULLUP);
 }
 
 void loop() {
@@ -22,14 +21,22 @@ void loop() {
   if (triggerPressed != triggerPrev) {
     Serial.print("Trigger state changed: ");
     Serial.println(triggerPressed);
-    if (triggerPressed) {
-      int detectorValue = digitalRead(inPinDetector);
+    /*if (triggerPressed) {
+      int detectorValue = analogRead(inPinDetector);
       
       Serial.print("Detector: ");
       Serial.println(detectorValue);
-    }
+    }*/
   }
-    
+  if (triggerPressed) {
+    int detectorPrev = detectorValue;
+    detectorValue = analogRead(inPinDetector);    
+    Serial.print("     Detector: ");
+    Serial.print(detectorValue);
+    Serial.print("   diff=");
+    Serial.println(detectorValue - detectorPrev);
+  }
+  
   int ledState = triggerPressed ? HIGH : LOW;
   digitalWrite(outPinLed, ledState);
 

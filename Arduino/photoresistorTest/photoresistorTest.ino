2fa08e33933ba4bc756c8c42fe3a5505d61217f8
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-10-29 18:55:44 +0300

    photodetectorTest: use short ring buffer and avg for input values

--- Arduino/photoresistorTest/photoresistorTest.ino
+++ Arduino/photoresistorTest/photoresistorTest.ino
@@ -1,3 +1,7 @@
+const int BUFFER_SIZE = 4;
+int m_buffer[BUFFER_SIZE];
+int m_bufIndex = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,7 +9,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int val = analogRead(A0);
+  int val = readValue();
   //Serial.print("Voltage");
   //Serial.print("\t");
   Serial.println(val);
@@ -14,3 +18,16 @@ void loop() {
   //we're interested in events that happen roughly within 15-20 ms
   
 }
+
+
+int readValue() {
+  m_buffer[m_bufIndex] = analogRead(A0);
+  m_bufIndex = (m_bufIndex + 1) % BUFFER_SIZE;
+  //test if median is better than avg
+  int sum = 0;
+  for (int n = 0; n < BUFFER_SIZE; ++n) {
+    sum += m_buffer[n];
+  }
+  return sum / BUFFER_SIZE;
+}
+

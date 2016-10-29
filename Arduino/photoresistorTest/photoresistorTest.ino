62be5343bc63feb47658d77460cb2f3ae644d0c6
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-10-29 18:40:01 +0300

    photoresistor test code

---
+++ Arduino/photoresistorTest/photoresistorTest.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(A0);
+  //Serial.print("Voltage");
+  //Serial.print("\t");
+  Serial.println(val);
+  delay(5);
+
+  //we're interested in events that happen roughly within 15-20 ms
+  
+}

bc896e0bb312a29e1475041ee0e4f51514ec0be5
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-10-30 14:48:17 +0200

    First version of testing if it's a hit or miss, based on timed gradients after trigger pull

--- Arduino/photoresistorTest/photoresistorTest.ino
+++ Arduino/photoresistorTest/photoresistorTest.ino
@@ -1,20 +1,36 @@
 const int BUFFER_SIZE = 4;
+/** Input */
 int m_buffer[BUFFER_SIZE];
 int m_bufIndex = 0;
 
 const int PIN_TRIGGER_INPUT = 2; // interrupt 0 is associated with pin 2
 
+/** Output */
 const int OUTPUT_SAMPLE_PERIOD_MS = 17;  // 60Hz = 17ms
 unsigned long m_outputTimer = 0;
 
+const bool SEND_ALWAYS = false; //debug flag
+
+/** State */
 enum State {
   Idle, Triggered
 };
 State m_state = Idle;
-const int TRIGGER_STATE_LENGTH_MS = 500;  // Burst length
+
+//we might need to add some detection to the third frame also, spike from white target to normal.
+enum HitState {
+  StartDetecting, BlankDetected, Miss, Hit
+};
+HitState m_hitState = StartDetecting;
+
+const int DETECTOR_THRESHOLD_BLANK = 10; // This needs testing. Optimally we would have an input "knob" where we could adjust it in runtime
+const int DETECTOR_THRESHOLD_TARGET = -5; // This needs testing. Optimally we would have an input "knob" where we could adjust it in runtime
+int m_prevDetector = 0;
+
+const int TRIGGER_STATE_LENGTH_MS = 200;  // Burst length. TODO reduce to reasonable, test how fast user can pull
 unsigned long m_triggeredTimer = 0;
 
-const bool SEND_ALWAYS = false; //debug flag
+/** Implementation begins */
 
 void setup() {
   Serial.begin(9600);
@@ -34,7 +50,9 @@ void setup() {
   attachInterrupt(0, triggerCallback, FALLING);
 }
 
-// Interrupt is called once a millisecond (Timer0 frequency)
+/** This is the actual main loop. 
+    Interrupt is called once a millisecond (Timer0 frequency) 
+  */
 SIGNAL(TIMER0_COMPA_vect) 
 {
   readSample();
@@ -47,13 +65,21 @@ SIGNAL(TIMER0_COMPA_vect)
 void loop() {
 }
 
+/** Trigger pressed, we need to start monitoring the state(s) and sending output */
 void triggerCallback() {
+  if (m_state == Triggered) {
+    Serial.println("Alredy Triggered..");
+    return;
+  }
   m_state = Triggered;
+  m_hitState = StartDetecting;
   m_triggeredTimer =  millis();
+  m_prevDetector = readBuffer();
+  Serial.println("TRIGGER");
 }
 
 void updateState() {
-  if (m_state == Triggered) {
+  if (m_state == Triggered) {    
     if (millis() - m_triggeredTimer > TRIGGER_STATE_LENGTH_MS) {
       m_state = Idle;
     }
@@ -65,26 +91,75 @@ void updateState() {
   // TODO raise led output when in triggered state
 }
 
+/**
+ * Detects the hits by looking at changes in input. We have a pullup resistor on detector
+ * so black equals positive spike, white as negative.
+ */
+void detectHit() {
+  int detector = readBuffer();
+  int diffToPrev = detector - m_prevDetector;
+  bool stateChanged = false;
+  
+  if (m_hitState == StartDetecting) {
+    //looking for black screen => spike upwards
+    if (diffToPrev >= DETECTOR_THRESHOLD_BLANK) {
+      m_hitState = BlankDetected;
+      stateChanged = true;
+      Serial.print("TO BLANK DETECTED STATE, diff:");
+      Serial.println(diffToPrev);
+    }
+  }
+  else if (m_hitState == BlankDetected) {
+    //looking for white target => spike downwards
+    if (diffToPrev <= DETECTOR_THRESHOLD_TARGET) {
+      m_hitState = Hit;
+      Serial.print("=> HIT! diff:");
+      Serial.println(diffToPrev);
+    }
+    else {
+      m_hitState = Miss;
+      Serial.print("<= MISS... diff:");
+      Serial.println(diffToPrev);
+    }
+    stateChanged = true;
+  }
+  
+  if (!stateChanged) {
+    Serial.print("no change, diff:");
+    Serial.println(diffToPrev);
+  }
+
+  
+  
+  m_prevDetector = detector;
+}
+
 void sendOutput()  {
   unsigned long curTime = millis();
 
   bool isSendState = m_state == Triggered || SEND_ALWAYS;
   if (isSendState) {
+    
     if (curTime - m_outputTimer >= OUTPUT_SAMPLE_PERIOD_MS) {
       m_outputTimer = curTime;
       
       int val = readBuffer();
       Serial.println(val);
+      //we might have to start the counter for detectHit() from the actual trigger..
+      //other option is not to be so super strict but allow some lag for the detection. might be more robust 
+      detectHit();
     }  
   }
 }
 
+/** Read single sample. Do not send this out */
 int readSample() {
   int value = m_buffer[m_bufIndex] = analogRead(A0);
   m_bufIndex = (m_bufIndex + 1) % BUFFER_SIZE;
   return value;
 }
 
+/** Use this for actual logic and output */
 int readBuffer() {
   //TODO: test if median is better than avg
   int sum = 0;

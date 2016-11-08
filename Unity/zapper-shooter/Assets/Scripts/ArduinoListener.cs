456cf86841e05b3bfb74be04bcc6128791edd0ef
Markus Paasovaara <markus.paasovaara@futurice.com>
2016-11-08 07:37:33 +0200

    Read serial port in separate thread

--- Unity/zapper-shooter/Assets/Scripts/ArduinoListener.cs
+++ Unity/zapper-shooter/Assets/Scripts/ArduinoListener.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO.Ports;
+using System.Threading;
 
 /**
  * For System.IO.Ports namespace you need to enable .Net 2.0 instead of .Net 2.0 subset (which is default).
@@ -36,19 +37,49 @@ public class ArduinoListener : MonoBehaviour {
         m_serial.Open();
         if (m_serial.IsOpen) {
             Debug.Log("Serial port opened " + comPort);
+
+            Thread t = new Thread(new ThreadStart(serialLoop));
+            t.Start();
+            //TODO stop thread and close serial port while closing the app
         }
         else {
             Debug.LogError("Failed to open serial port " + comPort);
         }
 
 	}
+
+    public void OnDestroy() {
+        //clean all resources
+        m_serial.Close();//Will this exit the thread loop also? wait for the thread..?
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (m_serial.IsOpen) {
+        /*if (m_serial.IsOpen) {
             Debug.Log("Reading serial port");
             string input = m_serial.ReadLine();//by default this blocks, so put this to separate thread, not to main loop
-        }
+        }*/
 
 	}
+
+    public void serialLoop() {
+        Debug.Log("Starting serial loop");
+
+        while (m_serial.IsOpen) {
+            Debug.Log("Reading new message");
+            //readline blocks until EOL
+            string input = m_serial.ReadLine();
+            handleMessage(input);
+
+
+            //TODO exit loop if closed. might require enabling timeouts
+
+        }
+        Debug.Log("Stopping serial loop");
+
+    }
+
+    private void handleMessage(string msg) {
+        Debug.Log("Read message: " + msg);
+    }
 }

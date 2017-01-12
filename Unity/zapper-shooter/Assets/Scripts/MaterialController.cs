67a56c66f15681fa54253cf6b2b78036527c5fe6
Markus Paasovaara <markus.paasovaara@futurice.com>
2017-01-12 07:51:02 +0200

    Change material on Zombie at runtime, to enable zapper logic

---
+++ Unity/zapper-shooter/Assets/Scripts/MaterialController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialController : MonoBehaviour {
+
+    [SerializeField]
+    private Material _Idle;
+
+    [SerializeField]
+    private Material _OnPress;
+
+
+    // Use this for initialization
+    void Start () {
+        setMaterial(_Idle);
+	}
+
+    private void setMaterial(Material m) {
+        //perhaps not the fastest operation? TODO optimize
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach(Renderer r in renderers) {
+            r.material = m;
+        }
+    }
+
+	// Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            setMaterial(_OnPress);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            setMaterial(_Idle);
+        }
+    }
+}

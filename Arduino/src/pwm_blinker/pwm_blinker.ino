59758b782112ae7dc667bd04aaa23c7eaa44051d
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-03-28 19:51:02 +0300

    Saving random files from old laptop. delete branch if not needed

---
+++ Arduino/src/pwm_blinker/pwm_blinker.ino
@@ -0,0 +1,54 @@
+const int outPinLed = 2;
+const int inPinTrigger = 7;
+const int inPinDetector = 4;
+
+int triggerPressed = 0;
+int detectorValue = 1023;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  pinMode(outPinLed, OUTPUT);
+  digitalWrite(outPinLed, LOW);
+
+  pinMode(inPinTrigger, INPUT_PULLUP);
+  pinMode(inPinDetector, INPUT_PULLUP);
+}
+
+void loop() {
+  int ledState = LOW;
+  while(true) {
+    readDetector();
+    
+    digitalWrite(outPinLed, ledState);
+    ledState = ledState == LOW ? HIGH : LOW;
+    //delay(3);
+    int triggerPrev = triggerPressed;
+    triggerPressed = digitalRead(inPinTrigger);
+    if (triggerPressed != triggerPrev) {
+      Serial.print("Trigger state changed: ");
+      Serial.println(triggerPressed);
+      if (triggerPressed == LOW) {
+        digitalWrite(outPinLed, LOW);
+        delay(15);
+        digitalWrite(outPinLed, HIGH);
+        delay(15);
+        digitalWrite(outPinLed, LOW);
+        
+      }
+    }
+  }
+}
+
+void readDetector() {
+  int detectorPrev = detectorValue;
+  detectorValue = digitalRead(inPinDetector);    
+  if (detectorValue != detectorPrev) {
+    Serial.print("     Detector Changed!: ");
+    Serial.println(detectorValue);
+    
+  }
+  
+}
+
---
+++ Arduino/src/zapper2/zapper2.ino
@@ -0,0 +1,67 @@
+const int outPinLed = 2;
+const int inPinTrigger = 7;
+const int inPinDetector = 4;
+
+int triggerPressed = 0;
+int detectorValue = 1023;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  pinMode(outPinLed, OUTPUT);
+  digitalWrite(outPinLed, LOW);
+
+  pinMode(inPinTrigger, INPUT_PULLUP);
+  pinMode(inPinDetector, INPUT_PULLUP);
+}
+
+void loop() {
+  int triggerPrev = triggerPressed;
+  triggerPressed = digitalRead(inPinTrigger);
+  if (triggerPressed != triggerPrev) {
+    Serial.print("Trigger state changed: ");
+    Serial.println(triggerPressed);
+  /*  if (triggerPressed == LOW) {
+      readDetector();
+      digitalWrite(outPinLed, LOW);
+      delay(15);
+      readDetector();
+      digitalWrite(outPinLed, HIGH);
+      delay(15);
+      readDetector();
+      digitalWrite(outPinLed, LOW);
+      delay(15);
+      readDetector();
+      
+    }*/
+  }
+  readDetector();
+  digitalWrite(outPinLed, HIGH);
+  
+  /*
+  if (triggerPressed == LOW) {
+    int detectorPrev = detectorValue;
+    detectorValue = analogRead(inPinDetector);    
+    Serial.print("     Detector: ");
+    Serial.print(detectorValue);
+    Serial.print("   diff=");
+    Serial.println(detectorValue - detectorPrev);
+  }
+  int ledState = !triggerPressed ? HIGH : LOW;
+  digitalWrite(outPinLed, ledState);
+*/
+  
+}
+
+void readDetector() {
+  int detectorPrev = detectorValue;
+  detectorValue = digitalRead(inPinDetector);    
+  if (detectorValue != detectorPrev) {
+    Serial.print("     Detector Changed!: ");
+    Serial.println(detectorValue);
+    
+  }
+  
+}
+
